1c1de0674f3d45e4919b5d3a2b955dab5e7f762b
Yurakaii <danndem@gmail.com>
2024-11-15 12:57:03 -0500

    Setup window

--- src/gitview/gitview.go
+++ src/gitview/gitview.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
 func main() {
+	a := app.New()
+	mainWindow := a.NewWindow("GitView")
+
+	mainWindow.Resize(fyne.NewSize(250,550))
+	mainWindow.Show()
+	a.Run()
 }
